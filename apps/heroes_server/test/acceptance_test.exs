47c8b9d6cad2edea6803b63f6dabe4f2895bcb93
Miguel C <miquecg@users.noreply.github.com>
2020-04-17 14:29:27 +0200

    Add failing test scenario: player joins the game

---
+++ apps/heroes_server/test/acceptance_test.exs
@@ -0,0 +1,19 @@
+defmodule HeroesServer.AcceptanceTest do
+  use ExUnit.Case, async: true
+
+  test "When a player joins the game a new hero is placed on a random walkable tile" do
+    assert active_heroes() == 0
+
+    Application.put_env(:heroes_server, :board, Boards.TwoTiles)
+    {_, {x_axis, y_axis} = _tile} = HeroesServer.join()
+
+    assert active_heroes() == 1
+    assert x_axis == 1
+    assert y_axis == 0
+  end
+
+  defp active_heroes() do
+    %{active: heroes} = DynamicSupervisor.count_children(Heroes.Supervisor)
+    heroes
+  end
+end
