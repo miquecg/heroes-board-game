12324f6c43d41199d9b4b709bada67c17c219fdd
Miguel C <miquecg@users.noreply.github.com>
2020-04-26 11:38:18 +0200

    Add new boards for testing

--- apps/heroes_server/test/helpers/board.ex
+++ apps/heroes_server/test/helpers/board.ex
@@ -1,5 +1,8 @@
-defmodule Board.Test do
-  @moduledoc false
+defmodule Board.Test_2x2 do
+  @moduledoc """
+  Size: 2x2
+  Walls: 1
+  """
 
   @board_spec %Board.Spec{
     cols: 2,
@@ -9,5 +12,88 @@ defmodule Board.Test do
 
   @tiles Board.tiles(@board_spec)
 
+  @doc """
+  ## Example
+  +---+---+
+  |   |   |
+  +---+---+
+  | W |   |
+  +---+---+
+  """
+  def tiles, do: @tiles
+end
+
+defmodule Board.Test_4x4 do
+  @moduledoc """
+  Size: 4x4
+  Walls: 0
+  """
+
+  @board_spec %Board.Spec{
+    cols: 4,
+    rows: 4,
+    walls: []
+  }
+
+  @tiles Board.tiles(@board_spec)
+
+  def tiles, do: @tiles
+end
+
+defmodule Board.Test_4x4_1 do
+  @moduledoc """
+  Size: 4x4
+  Walls: 1
+  """
+
+  @board_spec %Board.Spec{
+    cols: 4,
+    rows: 4,
+    walls: [{3, 2}]
+  }
+
+  @tiles Board.tiles(@board_spec)
+
+  @doc """
+  ## Example
+  +---+---+---+---+
+  |   | ⠀ |   | ⠀ |
+  +---+---+---+---+
+  |   | ⠀ |   | W |
+  +---+---+---+---+
+  |   |   |   |   |
+  +---+---+---+---+
+  |   |   |   |   |
+  +---+---+---+---+
+  """
+  def tiles, do: @tiles
+end
+
+defmodule Board.Test_4x4_2 do
+  @moduledoc """
+  Size: 4x4
+  Walls: 2
+  """
+
+  @board_spec %Board.Spec{
+    cols: 4,
+    rows: 4,
+    walls: [{1, 2}, {3, 2}]
+  }
+
+  @tiles Board.tiles(@board_spec)
+
+  @doc """
+  ## Example
+  +---+---+---+---+
+  |   | ⠀ |   | ⠀ |
+  +---+---+---+---+
+  |   | W |   | W |
+  +---+---+---+---+
+  |   |   |   |   |
+  +---+---+---+---+
+  |   |   |   |   |
+  +---+---+---+---+
+  """
   def tiles, do: @tiles
 end
