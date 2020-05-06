98206099425889982de179de8b01d83f5c753e93
Miguel C <miquecg@users.noreply.github.com>
2020-05-06 11:40:14 +0200

    Add test for attack range checks

--- apps/heroes_server/test/board_test.exs
+++ apps/heroes_server/test/board_test.exs
@@ -2,6 +2,8 @@ defmodule Game.BoardTest do
   use ExUnit.Case, async: true
   doctest Game.Board
 
+  alias Game.BoardRange
+
   @board GameBoards.Test4x4
 
   @doc """
@@ -31,6 +33,38 @@ defmodule Game.BoardTest do
   test "Anything other than a t:Game.Board.tile/0 is invalid" do
     refute valid?({0.5, 1})
     refute valid?({:an_atom, "a string"})
+  end
+
+  describe "Attack range from tile" do
+    setup %{tile: tile} do
+      range = @board.attack_range(tile)
+      [member_fn: fn point -> BoardRange.member?(range, point) end]
+    end
+
+    @doc """
+    +---+---+---+---+
+    | X | ⠀ | ⠀ | X |
+    +---+---+---+---+
+    | ✓ | ⠀ | ✓ |   |
+    +---+---+---+---+
+    |   | ✓ | ✓ | X |
+    +---+---+---+---+
+    | ✓ |   |   |   |
+    +---+---+---+---+
+    """
+    @tag tile: {1, 1}
+    test "{1, 1}", %{member_fn: in_range?} do
+      assert in_range?.({0, 0})
+      assert in_range?.({1, 1})
+      assert in_range?.({2, 2})
+      refute in_range?.({3, 3})
+
+      assert in_range?.({2, 1})
+      refute in_range?.({3, 1})
+
+      assert in_range?.({0, 2})
+      refute in_range?.({0, 3})
+    end
   end
 
   defp valid?(point) do
