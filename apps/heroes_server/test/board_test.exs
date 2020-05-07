bbd53e3e6026cddfadeaea203e75446c738155c7
Miguel C <miquecg@users.noreply.github.com>
2020-05-07 11:05:17 +0200

    One more test for checking ranges

--- apps/heroes_server/test/board_test.exs
+++ apps/heroes_server/test/board_test.exs
@@ -65,6 +65,30 @@ defmodule Game.BoardTest do
       assert in_range?.({0, 2})
       refute in_range?.({0, 3})
     end
+
+    @doc """
+                  X
+    +---+---+---+---+
+    | ⠀ | X | ✓ | ✓ |
+    +---+---+---+---+
+    | ⠀ | ⠀ | ✓ | ✓ |
+    +---+---+---+---+
+    |   |   |   | X |
+    +---+---+---+---+
+    |   |   |   |   |
+    +---+---+---+---+
+    """
+    @tag tile: {3, 3}
+    test "{3, 3}", %{member_fn: in_range?} do
+      assert in_range?.({2, 2})
+      assert in_range?.({2, 3})
+      assert in_range?.({3, 2})
+      assert in_range?.({3, 3})
+
+      refute in_range?.({1, 3})
+      refute in_range?.({3, 1})
+      refute in_range?.({3, 4})
+    end
   end
 
   defp valid?(point) do
