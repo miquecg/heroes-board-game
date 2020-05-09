8e4f8e4ddd0a4bd049440373da501d1f7e099628
Miguel C <miquecg@users.noreply.github.com>
2020-05-09 13:34:27 +0200

    Move BoardRange tests to its own module

--- apps/heroes_server/test/board_test.exs
+++ apps/heroes_server/test/board_test.exs
@@ -2,8 +2,6 @@ defmodule Game.BoardTest do
   use ExUnit.Case, async: true
   doctest Game.Board
 
-  alias Game.BoardRange
-
   @board_3x2 GameBoards.Test3x2
   @board_4x4 GameBoards.Test4x4
   @board_4x4_w2 GameBoards.Test4x4w2
@@ -123,62 +121,6 @@ defmodule Game.BoardTest do
 
       assert valid?.({2, 0})
       refute valid?.({3, 0})
-    end
-  end
-
-  describe "Attack range from tile" do
-    setup %{tile: tile} do
-      range = @board_4x4.attack_range(tile)
-      [member_fn: fn point -> BoardRange.member?(range, point) end]
-    end
-
-    @doc """
-    +---+---+---+---+
-    | X | ⠀ | ⠀ | X |
-    +---+---+---+---+
-    | ✓ | ⠀ | ✓ |   |
-    +---+---+---+---+
-    |   | ✓ | ✓ | X |
-    +---+---+---+---+
-    | ✓ |   |   |   |
-    +---+---+---+---+
-    """
-    @tag tile: {1, 1}
-    test "{1, 1}", %{member_fn: in_range?} do
-      assert in_range?.({0, 0})
-      assert in_range?.({1, 1})
-      assert in_range?.({2, 2})
-      refute in_range?.({3, 3})
-
-      assert in_range?.({2, 1})
-      refute in_range?.({3, 1})
-
-      assert in_range?.({0, 2})
-      refute in_range?.({0, 3})
-    end
-
-    @doc """
-                  X
-    +---+---+---+---+
-    | ⠀ | X | ✓ | ✓ |
-    +---+---+---+---+
-    | ⠀ | ⠀ | ✓ | ✓ |
-    +---+---+---+---+
-    |   |   |   | X |
-    +---+---+---+---+
-    |   |   |   |   |
-    +---+---+---+---+
-    """
-    @tag tile: {3, 3}
-    test "{3, 3}", %{member_fn: in_range?} do
-      assert in_range?.({2, 2})
-      assert in_range?.({2, 3})
-      assert in_range?.({3, 2})
-      assert in_range?.({3, 3})
-
-      refute in_range?.({1, 3})
-      refute in_range?.({3, 1})
-      refute in_range?.({3, 4})
     end
   end
 
