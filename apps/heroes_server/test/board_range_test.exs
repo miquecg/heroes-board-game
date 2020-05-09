537d8fddcd183fc6a4f4ee88fedf556df574271b
Miguel C <miquecg@users.noreply.github.com>
2020-05-09 13:54:11 +0200

    Move tests out of describe block

--- apps/heroes_server/test/board_range_test.exs
+++ apps/heroes_server/test/board_range_test.exs
@@ -5,59 +5,57 @@ defmodule Game.BoardRangeTest do
 
   @board_4x4 GameBoards.Test4x4
 
-  describe "Attack range from tile" do
-    setup %{tile: tile} do
-      range = @board_4x4.attack_range(tile)
-      [member_fn: &BoardRange.member?(range, &1)]
-    end
+  setup %{tile: tile} do
+    range = @board_4x4.attack_range(tile)
+    [member_fn: &BoardRange.member?(range, &1)]
+  end
 
-    @doc """
-    +---+---+---+---+
-    | X | ⠀ | ⠀ | X |
-    +---+---+---+---+
-    | ✓ | ⠀ | ✓ |   |
-    +---+---+---+---+
-    |   | ✓ | ✓ | X |
-    +---+---+---+---+
-    | ✓ |   |   |   |
-    +---+---+---+---+
-    """
-    @tag tile: {1, 1}
-    test "{1, 1}", %{member_fn: in_range?} do
-      assert in_range?.({0, 0})
-      assert in_range?.({1, 1})
-      assert in_range?.({2, 2})
-      refute in_range?.({3, 3})
+  @doc """
+  +---+---+---+---+
+  | X | ⠀ | ⠀ | X |
+  +---+---+---+---+
+  | ✓ | ⠀ | ✓ |   |
+  +---+---+---+---+
+  |   | ✓ | ✓ | X |
+  +---+---+---+---+
+  | ✓ |   |   |   |
+  +---+---+---+---+
+  """
+  @tag tile: {1, 1}
+  test "Attack ranges span one tile on every direction", %{member_fn: in_range?} do
+    assert in_range?.({0, 0})
+    assert in_range?.({1, 1})
+    assert in_range?.({2, 2})
+    refute in_range?.({3, 3})
 
-      assert in_range?.({2, 1})
-      refute in_range?.({3, 1})
+    assert in_range?.({0, 2})
+    refute in_range?.({0, 3})
 
-      assert in_range?.({0, 2})
-      refute in_range?.({0, 3})
-    end
+    assert in_range?.({2, 1})
+    refute in_range?.({3, 1})
+  end
 
-    @doc """
-                  X
-    +---+---+---+---+
-    | ⠀ | X | ✓ | ✓ |
-    +---+---+---+---+
-    | ⠀ | ⠀ | ✓ | ✓ |
-    +---+---+---+---+
-    |   |   |   | X |
-    +---+---+---+---+
-    |   |   |   |   |
-    +---+---+---+---+
-    """
-    @tag tile: {3, 3}
-    test "{3, 3}", %{member_fn: in_range?} do
-      assert in_range?.({2, 2})
-      assert in_range?.({2, 3})
-      assert in_range?.({3, 2})
-      assert in_range?.({3, 3})
+  @doc """
+                X
+  +---+---+---+---+
+  | ⠀ | X | ✓ | ✓ |
+  +---+---+---+---+
+  | ⠀ | ⠀ | ✓ | ✓ |
+  +---+---+---+---+
+  |   |   |   | X |
+  +---+---+---+---+
+  |   |   |   |   |
+  +---+---+---+---+
+  """
+  @tag tile: {3, 3}
+  test "Attack ranges are scoped to board limits", %{member_fn: in_range?} do
+    assert in_range?.({2, 2})
+    assert in_range?.({2, 3})
+    assert in_range?.({3, 2})
+    assert in_range?.({3, 3})
 
-      refute in_range?.({1, 3})
-      refute in_range?.({3, 1})
-      refute in_range?.({3, 4})
-    end
+    refute in_range?.({1, 3})
+    refute in_range?.({3, 1})
+    refute in_range?.({3, 4})
   end
 end
