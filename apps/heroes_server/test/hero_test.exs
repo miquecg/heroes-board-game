ea4c1ffcc6458efce1ce6d29d3b009b730220001
Miguel C <miquecg@users.noreply.github.com>
2020-05-10 12:19:37 +0200

    Rearrange hero tests

--- apps/heroes_server/test/hero_test.exs
+++ apps/heroes_server/test/hero_test.exs
@@ -8,27 +8,65 @@ defmodule Game.HeroTest do
   @board_4x4_w1 GameBoards.Test4x4w1
   @board_4x4_w2 GameBoards.Test4x4w2
 
-  describe "A hero can move one tile" do
+  describe "A hero can move in four directions one tile at a time:" do
     setup :create_hero
 
-    test "up", %{hero: pid} do
+    test "go up", %{hero: pid} do
       assert {:ok, {1, 2}} = Hero.control(pid, :up)
     end
 
-    test "down", %{hero: pid} do
+    test "go down", %{hero: pid} do
       assert {:ok, {1, 0}} = Hero.control(pid, :down)
     end
 
-    test "right", %{hero: pid} do
+    test "go right", %{hero: pid} do
       assert {:ok, {2, 1}} = Hero.control(pid, :right)
     end
 
-    test "left", %{hero: pid} do
+    test "go left", %{hero: pid} do
       assert {:ok, {0, 1}} = Hero.control(pid, :left)
     end
   end
 
-  describe "Heroes return error when commands are" do
+  describe "A hero can go anywhere on the board but not crossing walls:" do
+    setup do
+      [
+        tile: {2, 1},
+        commands: [:down, :right, :up, :up, :left, :up, :left, :left, :down, :down]
+      ]
+    end
+
+    setup :create_hero
+
+    test "route without walls", %{hero: pid} = context do
+      assert {0, 1} = control(pid, context.commands)
+    end
+
+    @tag board: @board_4x4_w1
+    test "same route with one wall", %{hero: pid} = context do
+      assert {0, 0} = control(pid, context.commands)
+    end
+
+    @tag board: @board_4x4_w2
+    test "same route with two walls", %{hero: pid} = context do
+      assert {2, 0} = control(pid, context.commands)
+    end
+  end
+
+  describe "A hero on tile {1, 1} dies when attacked by an enemy on tile" do
+    setup :create_hero
+
+    setup %{board: board, enemy: tile} do
+      [range: board.attack_range(tile)]
+    end
+
+    @tag enemy: {0, 0}
+    test "{0, 0}", %{hero: pid} = context do
+      assert {:ok, :dead} = GenServer.call(pid, {:attack, context.range})
+    end
+  end
+
+  describe "A hero returns error when commands are" do
     setup :create_hero
 
     test "invalid atoms", %{hero: pid} do
@@ -44,46 +82,8 @@ defmodule Game.HeroTest do
     end
   end
 
-  describe "Heroes can move to any tile but not crossing walls:" do
-    setup do
-      [
-        tile: {2, 1},
-        commands: [:down, :right, :up, :up, :left, :up, :left, :left, :down, :down]
-      ]
-    end
-
-    setup :create_hero
-
-    test "route without walls", %{hero: pid} = context do
-      assert {0, 1} = control(pid, context.commands)
-    end
-
-    @tag board: @board_4x4_w1
-    test "route with one wall", %{hero: pid} = context do
-      assert {0, 0} = control(pid, context.commands)
-    end
-
-    @tag board: @board_4x4_w2
-    test "route with two walls", %{hero: pid} = context do
-      assert {2, 0} = control(pid, context.commands)
-    end
-  end
-
   test "Heroes are temporary workers" do
     assert Supervisor.child_spec(Hero, []).restart == :temporary
-  end
-
-  describe "A hero on tile {1, 1} dies when attacked by an enemy on tile" do
-    setup :create_hero
-
-    setup %{board: board, enemy: tile} do
-      [range: board.attack_range(tile)]
-    end
-
-    @tag enemy: {0, 0}
-    test "{0, 0}", %{hero: pid} = context do
-      assert {:ok, :dead} = GenServer.call(pid, {:attack, context.range})
-    end
   end
 
   defp create_hero(context) do
