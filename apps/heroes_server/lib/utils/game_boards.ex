1ba9528c919ed5818d46a5e1c7e8eca0caf8230f
Miguel C <miquecg@users.noreply.github.com>
2020-06-08 17:44:54 +0200

    Define callbacks that boards are implementing

--- apps/heroes_server/lib/utils/game_boards.ex
+++ apps/heroes_server/lib/utils/game_boards.ex
@@ -3,8 +3,13 @@ defmodule Utils.GameBoards do
   Provide functionality for boards with use macro.
   """
 
-  alias Game.Board
+  alias Game.{Board, BoardRange}
   alias GameError.BadSize
+
+  @callback spec :: Board.t()
+  @callback tiles :: list(Board.tile())
+  @callback attack_range(Board.tile()) :: BoardRange.t()
+  @callback play(Board.tile(), Board.moves()) :: Board.tile()
 
   @doc false
   defmacro __using__(opts) do
@@ -17,16 +22,22 @@ defmodule Utils.GameBoards do
     tiles = Board.generate(board_spec)
 
     quote do
+      @behaviour Utils.GameBoards
+
       alias Game.Board
 
       @board_spec unquote(Macro.escape(board_spec))
 
+      @impl true
       def spec, do: @board_spec
 
+      @impl true
       def tiles, do: unquote(tiles)
 
+      @impl true
       def attack_range(tile), do: Board.attack_range(tile, @board_spec)
 
+      @impl true
       def play(tile, move), do: Board.play(tile, move, @board_spec)
     end
   end
