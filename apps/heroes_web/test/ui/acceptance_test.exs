28749019e7403bacf65419a5bb7b8dcce2a7d330
Miguel C <miquecg@users.noreply.github.com>
2020-06-19 12:02:10 +0200

    Add feature test for loading the board grid

--- apps/heroes_web/test/ui/acceptance_test.exs
+++ apps/heroes_web/test/ui/acceptance_test.exs
@@ -13,4 +13,15 @@ defmodule HeroesWeb.AcceptanceTest do
     |> assert_has(css("h1", text: "Welcome to Heroes Board Game"))
     |> assert_has(css("p", text: "Coming soon..."))
   end
+
+  @game game_path(@endpoint, :index)
+
+  feature "When a browser opens the game endpoint the board grid is loaded", %{session: session} do
+    Application.put_env(:heroes_server, :board, GameBoards.Oblivion)
+
+    session
+    |> visit(@game)
+    |> assert_has(css("#grid .cell", count: 48))
+    |> assert_has(css(".cell.wall", count: 7))
+  end
 end
