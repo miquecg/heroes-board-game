ed19a4b381eeb8a51157261dbd4d0587d360b8df
Miguel C <miquecg@users.noreply.github.com>
2020-06-26 17:07:36 +0200

    Generate the HTML markup from the board spec

--- apps/heroes_web/lib/heroes_web/controllers/game_controller.ex
+++ apps/heroes_web/lib/heroes_web/controllers/game_controller.ex
@@ -2,6 +2,6 @@ defmodule HeroesWeb.GameController do
   use HeroesWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    render(conn, "index.html", board: GameBoards.Oblivion.spec())
   end
 end
