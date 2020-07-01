a80bf1f79e155becc0a4f3b9423d28b52afab306
Miguel C <miquecg@users.noreply.github.com>
2020-07-01 16:28:58 +0200

    Test feature that creates a hero after clicking on start button

--- apps/heroes_web/test/ui/acceptance_test.exs
+++ apps/heroes_web/test/ui/acceptance_test.exs
@@ -2,14 +2,24 @@ defmodule Web.AcceptanceTest do
   use HeroesWeb.BrowserCase, async: true
   @moduletag :browser
 
-  import Wallaby.Query, only: [css: 2]
+  import Wallaby.Query, only: [button: 1, css: 2]
 
   @game game_path(@endpoint, :index)
 
-  feature "When a browser opens the game endpoint the board grid is loaded", %{session: session} do
+  feature "User visits the game URL and the board grid is loaded", %{session: session} do
     session
     |> visit(@game)
     |> assert_has(css("#grid .cell", count: 48))
     |> assert_has(css(".cell.wall", count: 7))
   end
+
+  feature "User clicks the start button and their hero appears on the grid", %{session: session} do
+    session
+    |> visit(@game)
+    |> refute_has(hero_on_the_grid())
+    |> click(button("Start"))
+    |> assert_has(hero_on_the_grid(count: 1))
+  end
+
+  defp hero_on_the_grid(opts \\ []), do: css("#grid .hero", opts)
 end
