94d15a25246e9ba3626781a8e4e622ec4833f1b1
Miguel C <miquecg@users.noreply.github.com>
2020-07-08 16:40:20 +0200

    CSS styles for hero and start button

--- apps/heroes_web/lib/web/endpoint.ex
+++ apps/heroes_web/lib/web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule Web.Endpoint do
     at: "/",
     from: :heroes_web,
     gzip: false,
-    only: ~w(css favicon.ico robots.txt)
+    only: ~w(css images favicon.ico robots.txt)
 
   if code_reloading? do
     socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
