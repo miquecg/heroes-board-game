60062ea45969402d27ddae17bad58af17a73228a
Miguel C <miquecg@users.noreply.github.com>
2020-07-09 11:56:45 +0200

    Add test for GameView.rows/1

---
+++ apps/heroes_web/test/views/game_view_test.exs
@@ -0,0 +1,17 @@
+defmodule Web.GameViewTest do
+  use HeroesWeb.ConnCase, async: true
+
+  import Web.GameView
+  alias Game.Board
+
+  test "Count how many rows the board has" do
+    board = Board.new(rows: 3, cols: 1)
+    assert rows(board) == 3
+
+    board = Board.new(rows: 1, cols: 5)
+    assert rows(board) == 1
+
+    board = Board.new(rows: 6, cols: 4, walls: [{0, 0}, {1, 2}])
+    assert rows(board) == 6
+  end
+end
