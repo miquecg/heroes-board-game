a3e35ed29ff51522bfca007c6a485dc39ae4c030
Miguel C <miquecg@users.noreply.github.com>
2020-07-12 13:52:52 +0200

    Add feature test of rendering all heroes playing the game

--- apps/heroes_web/test/ui/acceptance_test.exs
+++ apps/heroes_web/test/ui/acceptance_test.exs
@@ -1,5 +1,5 @@
 defmodule Web.AcceptanceTest do
-  use HeroesWeb.BrowserCase, async: true
+  use HeroesWeb.BrowserCase
   @moduletag :browser
 
   import Wallaby.Query, only: [button: 1, css: 2]
@@ -13,12 +13,30 @@ defmodule Web.AcceptanceTest do
     |> assert_has(css(".cell.wall", count: 7))
   end
 
-  feature "User clicks the start button and their hero appears on the grid", %{session: session} do
+  feature "User clicks the start button and their hero appears in the board grid", %{
+    session: session
+  } do
     session
     |> visit(@game)
     |> refute_has(hero_on_the_grid())
     |> click(button("Start"))
     |> assert_has(hero_on_the_grid(count: 1))
+  end
+
+  @sessions 2
+  feature "Other players' heroes appear in the board grid when loaded", %{
+    sessions: [user1, user2]
+  } do
+    user1
+    |> visit(@game)
+    |> click(button("Start"))
+
+    user2
+    |> visit(@game)
+    |> click(button("Start"))
+
+    user2
+    |> assert_has(hero_on_the_grid(count: 2))
   end
 
   defp hero_on_the_grid(opts \\ []), do: css("#grid .hero", opts)
