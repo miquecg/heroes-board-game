f802ac7874eebd2727e17d0f46f71a1acb8710c9
Miguel C <miquecg@users.noreply.github.com>
2020-07-28 14:00:59 +0200

    Let crash when joining game channel if Hero doesn't exist
    Clients must redirect the player to obtain a new valid token.

--- apps/heroes_web/lib/web/channels/game_channel.ex
+++ apps/heroes_web/lib/web/channels/game_channel.ex
@@ -10,23 +10,21 @@ defmodule Web.GameChannel do
 
   @impl true
   def join("game:lobby", _message, socket) do
-    send(self(), :after_join)
+    send(self(), {:after_join, player_position(socket)})
     {:ok, socket}
   end
 
   @impl true
-  def handle_info(:after_join, socket) do
-    server = hero_name(socket)
-    {x, y} = Game.Hero.position(server)
-
+  def handle_info({:after_join, {x, y}}, socket) do
     push(socket, "presence_state", Presence.list(socket))
     {:ok, _} = Presence.track(socket, socket.assigns.player_id, %{x: x, y: y})
 
     {:noreply, socket}
   end
 
-  @spec hero_name(Socket.t()) :: {:via, module(), term()}
-  defp hero_name(socket) do
-    {:via, Registry, {HeroesServer.Registry, socket.assigns.player_id}}
+  @spec player_position(Socket.t()) :: Game.tile()
+  defp player_position(socket) do
+    server = {:via, Registry, {HeroesServer.Registry, socket.assigns.player_id}}
+    Game.Hero.position(server)
   end
 end
