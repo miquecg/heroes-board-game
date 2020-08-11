9d4c2edb3f5abeb00c98cc22e3e7f1f7d8226ef0
Miguel C <miquecg@users.noreply.github.com>
2020-08-11 12:38:53 +0200

    Add failing test: channel broadcasts leave when hero is restarted

--- apps/heroes_web/test/channels/game_channel_test.exs
+++ apps/heroes_web/test/channels/game_channel_test.exs
@@ -1,5 +1,5 @@
 defmodule Web.GameChannelTest do
-  use HeroesWeb.ChannelCase, async: true
+  use HeroesWeb.ChannelCase
 
   alias Web.GameChannel
   alias Web.PlayerSocket, as: Socket
@@ -25,6 +25,17 @@ defmodule Web.GameChannelTest do
     assert_broadcast "presence_diff", %{joins: %{^id => _metas}, leaves: %{}}
   end
 
+  test "Channel broadcasts leave when a hero is restarted", context do
+    flush_messages()
+
+    Process.flag(:trap_exit, true)
+    %{player_id: id} = context.assigns
+    kill_hero(id)
+
+    assert_broadcast "presence_diff", %{joins: %{}, leaves: %{^id => _metas}}
+    assert_receive {:EXIT, _pid, :hero_down}
+  end
+
   test "Channel join crashes with an invalid player_id" do
     assigns = %{player_id: "invalid"}
     assert {:error, %{reason: "join crashed"}} = join_channel(assigns)
@@ -38,5 +49,24 @@ defmodule Web.GameChannelTest do
     Socket
     |> socket(nil, assigns)
     |> subscribe_and_join(GameChannel, "game:lobby")
+  end
+
+  def flush_messages(timeout \\ 100) do
+    receive do
+      %Phoenix.Socket.Message{} ->
+        flush_messages()
+
+      %Phoenix.Socket.Broadcast{} ->
+        flush_messages()
+    after
+      timeout -> nil
+    end
+  end
+
+  defp kill_hero(player_id) do
+    player_id
+    |> HeroesServer.hero_name()
+    |> GenServer.whereis()
+    |> Process.exit(:kill)
   end
 end
