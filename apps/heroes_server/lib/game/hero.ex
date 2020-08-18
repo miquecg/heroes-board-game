a57400ea93fd438d0e3cb0bac209bdd0be35f6b9
Miguel C <miquecg@users.noreply.github.com>
2020-08-18 18:16:12 +0200

    Remove module for aliasing other modules types

--- apps/heroes_server/lib/game/hero.ex
+++ apps/heroes_server/lib/game/hero.ex
@@ -19,7 +19,7 @@ defmodule Game.Hero do
 
   @typep state :: %__MODULE__.State{
            board: module(),
-           tile: Game.tile(),
+           tile: Board.tile(),
            alive: boolean()
          }
 
@@ -51,20 +51,18 @@ defmodule Game.Hero do
   @doc """
   Send a command to control a hero.
 
-  `server` is the hero reference.
-  `cmd` is of type `t:Game.moves/0` or atom `:attack`.
+  `server` is the hero reference and `cmd`
+  can be `t:Game.Board.moves/0` or atom `:attack`.
 
   Returns `{:ok, tile}`, `{:ok, :launched}` or
   `{:error, :noop}` when hero is dead and
-  cannot execute any command.
+  cannot execute any further actions.
 
   For invalid commands returns exception
   `GameError.BadCommand` in the error tuple.
   """
-  @spec control(GenServer.server(), term()) ::
-          {:ok, tile | :launched}
-          | {:error, error}
-        when tile: Game.tile(), error: :noop | %BadCommand{}
+  @spec control(GenServer.server(), term()) :: {:ok, tile | :launched} | {:error, error}
+        when tile: Board.tile(), error: :noop | %BadCommand{}
   def control(server, cmd)
 
   def control(server, cmd) when cmd in @moves, do: GenServer.call(server, {:play, cmd})
@@ -86,7 +84,7 @@ defmodule Game.Hero do
   @doc """
   Get current hero position.
   """
-  @spec position(GenServer.server()) :: Game.tile()
+  @spec position(GenServer.server()) :: Board.tile()
   def position(server), do: GenServer.call(server, :position)
 
   ## Server (callbacks)
@@ -157,7 +155,7 @@ defmodule Game.Hero do
     {:noreply, state}
   end
 
-  @spec stream_task(Game.tile(), list()) :: :done
+  @spec stream_task(Board.tile(), list()) :: :done
   def stream_task(tile, enemies) do
     opts = [ordered: false]
 
