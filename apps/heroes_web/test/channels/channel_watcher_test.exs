793b0efa5073decfd11a20e6bfaded34993828ec
Miguel C <miquecg@users.noreply.github.com>
2020-09-03 12:28:24 +0200

    Add failing test to implement removal of heroes after some time

---
+++ apps/heroes_web/test/channels/channel_watcher_test.exs
@@ -0,0 +1,25 @@
+defmodule Web.ChannelWatcherTest do
+  use HeroesWeb.ChannelCase
+
+  setup context do
+    {:ok, _, socket} = join(context.socket, @topics.board)
+
+    [socket: socket]
+  end
+
+  setup context do
+    [ref: Process.monitor(context.hero_pid)]
+  end
+
+  test "Hero is removed time after leaving the channel", %{ref: ref} = context do
+    leave_channel(context.socket)
+
+    assert_receive {:DOWN, ^ref, :process, _pid, :normal}
+  end
+
+  test "Hero is removed time after closing the socket", %{ref: ref} = context do
+    close_socket(context.socket)
+
+    assert_receive {:DOWN, ^ref, :process, _pid, :normal}
+  end
+end
