d2ce0c6e5fefadd7335bd5d4d78b49fe64ff0da1
Miguel C <miquecg@users.noreply.github.com>
2020-09-07 10:54:40 +0200

    Start ChannelWatcher for its own tests
    Also rise timeouts to avoid false positives.

--- apps/heroes_web/test/channels/channel_watcher_test.exs
+++ apps/heroes_web/test/channels/channel_watcher_test.exs
@@ -4,24 +4,25 @@ defmodule Web.ChannelWatcherTest do
   setup :register_hero
 
   setup context do
+    [ref: Process.monitor(context.hero_pid)]
+  end
+
+  setup context do
+    start_supervised!({Web.ChannelWatcher, reconnect_timeout: 50})
     {:ok, _, socket} = join(context.socket, @topics.board)
 
     [socket: socket]
   end
 
-  setup context do
-    [ref: Process.monitor(context.hero_pid)]
-  end
-
   test "Hero is removed time after leaving the channel", %{ref: ref} = context do
     leave_channel(context.socket)
 
-    assert_receive {:DOWN, ^ref, :process, _pid, :normal}, 150
+    assert_receive {:DOWN, ^ref, :process, _pid, :normal}, 600
   end
 
   test "Hero is removed time after closing the socket", %{ref: ref} = context do
     close_socket(context.socket)
 
-    assert_receive {:DOWN, ^ref, :process, _pid, :normal}, 150
+    assert_receive {:DOWN, ^ref, :process, _pid, :normal}, 600
   end
 end
