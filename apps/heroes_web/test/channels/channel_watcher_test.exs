8ced652c56e4a5d20bc0f08a9fdc340ef4d1aff7
Miguel C <miquecg@users.noreply.github.com>
2020-09-11 19:03:33 +0200

    Move Channels setup for testing to their respective modules

--- apps/heroes_web/test/channels/channel_watcher_test.exs
+++ apps/heroes_web/test/channels/channel_watcher_test.exs
@@ -1,44 +1,72 @@
 defmodule Web.ChannelWatcherTest do
   use HeroesWeb.ChannelCase
 
-  setup :register_hero
+  setup :start_hero
+  setup :monitor
 
   setup context do
-    [ref: Process.monitor(context.hero_pid)]
-  end
-
-  setup context do
-    start_supervised!({Web.ChannelWatcher, reconnect_timeout: 50})
+    timeout = Map.get(context, :reconnect, 50)
+    start_supervised!({Web.ChannelWatcher, reconnect_timeout: timeout})
     {:ok, _, socket} = join(context.socket, @topics.board)
 
     [socket: socket]
   end
 
-  test "Hero is removed time after leaving the channel", %{ref: ref} = context do
+  test "Hero is removed after leaving the channel", %{ref: ref} = context do
     leave_channel(context.socket)
 
     assert_receive {:DOWN, ^ref, :process, _pid, :normal}, 600
   end
 
-  test "Hero is removed time after closing the socket", %{ref: ref} = context do
+  test "Hero is removed after closing the socket", %{ref: ref} = context do
     close_socket(context.socket)
 
     assert_receive {:DOWN, ^ref, :process, _pid, :normal}, 600
   end
 
-  test "Hero is not removed when player reconnects before timeout", %{ref: ref} = context do
+  test "Hero is not removed when player reconnects during timeout", %{ref: ref} = context do
     leave_channel(context.socket)
     {:ok, _, _} = join(context.socket, @topics.board)
 
     refute_receive {:DOWN, ^ref, :process, _pid, :normal}, 1000
   end
 
-  test "Tracks joins/leaves correctly with more than one hero", %{ref: ref} = context do
-    second_socket = Keyword.get(dummy_hero(:two), :socket)
+  @tag reconnect: 100
+  test "Tracks different heroes correctly", %{ref: ref, socket: first_socket} do
+    leave_channel(first_socket)
 
-    leave_channel(context.socket)
+    second_socket = start_hero()
     {:ok, _, _} = join(second_socket, @topics.board)
 
     assert_receive {:DOWN, ^ref, :process, _pid, :normal}, 600
+  end
+
+  defp start_hero do
+    Web.PlayerSocket
+    |> socket()
+    |> start_hero()
+  end
+
+  defp start_hero(%{socket: socket}), do: [socket: start_hero(socket)]
+
+  defp start_hero(socket) do
+    id = Game.join()
+    hero = Game.hero(id)
+
+    assigns = %{
+      player_id: id,
+      hero: hero
+    }
+
+    Phoenix.Socket.assign(socket, assigns)
+  end
+
+  defp monitor(%{socket: socket}) do
+    ref =
+      socket.assigns.hero
+      |> GenServer.whereis()
+      |> Process.monitor()
+
+    [ref: ref]
   end
 end
--- apps/heroes_web/test/channels/game_channel_test.exs
+++ apps/heroes_web/test/channels/game_channel_test.exs
@@ -1,51 +1,46 @@
 defmodule Web.GameChannelTest do
   use HeroesWeb.ChannelCase, async: true
 
-  setup :dummy_hero
+  alias Game.HeroServer
 
-  describe "Player position is tracked on game:board topic." do
+  setup :start_hero
+
+  describe "Presence on game:board topic" do
     setup context do
-      [socket: subscribe_and_join!(context.socket, @topics.board)]
+      socket = subscribe_and_join!(context.socket, @topics.board)
+
+      [socket: socket, player_id: socket.assigns.player_id]
     end
 
-    setup %{socket: socket} do
-      [player_id: socket.assigns.player_id]
-    end
-
-    test "When joining channel presence_state is pushed and presence_diff broadcasted",
-         %{player_id: id} do
+    test "when joining channel", %{player_id: id} do
       assert_push "presence_state", %{}
 
       assert_broadcast "presence_diff", %{joins: %{^id => %{metas: metas}}, leaves: %{}}
       assert [%{phx_ref: _, x: 5, y: 3}] = metas
     end
 
-    test "When leaving channel presence_diff is broadcasted", %{player_id: id} = context do
+    test "when leaving channel", %{player_id: id} = context do
       leave_channel(context.socket)
 
       assert_broadcast "presence_diff", %{joins: %{}, leaves: %{^id => _metas}}
     end
 
-    test "When closing socket presence_diff is broadcasted", %{player_id: id} = context do
+    test "when closing socket", %{player_id: id} = context do
       close_socket(context.socket)
 
       assert_broadcast "presence_diff", %{joins: %{}, leaves: %{^id => _metas}}
     end
   end
 
-  describe "Player online presence is tracked on game:lobby topic." do
+  describe "Presence on game:lobby topic" do
     setup context do
       @endpoint.subscribe(@topics.lobby)
       {:ok, _, socket} = join(context.socket, @topics.board)
 
-      [socket: socket]
+      [socket: socket, player_id: socket.assigns.player_id]
     end
 
-    setup %{socket: socket} do
-      [player_id: socket.assigns.player_id]
-    end
-
-    test "When joining channel presence_diff is broadcasted", %{player_id: id} do
+    test "when joining channel", %{player_id: id} do
       assert_receive %Phoenix.Socket.Broadcast{
         event: "presence_diff",
         payload: %{
@@ -56,7 +51,7 @@ defmodule Web.GameChannelTest do
       }
     end
 
-    test "When leaving channel presence_diff is broadcasted", %{player_id: id} = context do
+    test "when leaving channel", %{player_id: id} = context do
       leave_channel(context.socket)
 
       assert_receive %Phoenix.Socket.Broadcast{
@@ -69,7 +64,7 @@ defmodule Web.GameChannelTest do
       }
     end
 
-    test "When closing socket presence_diff is broadcasted", %{player_id: id} = context do
+    test "when closing socket", %{player_id: id} = context do
       close_socket(context.socket)
 
       assert_receive %Phoenix.Socket.Broadcast{
@@ -83,15 +78,31 @@ defmodule Web.GameChannelTest do
     end
   end
 
-  test "A player cannot join if the hero is no longer active", context do
-    stop_supervised!(Game.Hero)
+  test "Join crashes if the hero is no longer active", context do
+    stop_supervised!(HeroServer)
 
     assert {:error, %{reason: "join crashed"}} = join(context.socket, @topics.board)
   end
 
-  test "A tracked player cannot join simultaneously on different socket connections", context do
-    {:ok, _, _} = join(context.socket, @topics.board)
+  test "Join unauthorized on a second connection for the same player", %{socket: socket} do
+    {:ok, _, _} = join(socket, @topics.board)
 
-    assert {:error, %{reason: "unauthorized"}} = join(context.socket, @topics.board)
+    assert {:error, %{reason: "unauthorized"}} = join(socket, @topics.board)
+  end
+
+  defp start_hero(%{test: name, socket: socket}) do
+    opts = [
+      name: name,
+      board: nil,
+      tile: {5, 3}
+    ]
+    start_supervised!({HeroServer, opts})
+
+    assigns = %{
+      player_id: Atom.to_string(name),
+      hero: name
+    }
+
+    [socket: Phoenix.Socket.assign(socket, assigns)]
   end
 end
