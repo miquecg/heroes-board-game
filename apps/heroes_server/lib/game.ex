1a92000c99caee32aacf2a00cb64d8089c7d0e89
Miguel C <miquecg@users.noreply.github.com>
2020-09-18 12:57:09 +0200

    Rewrite Game client without GenServer

--- apps/heroes_server/lib/game.ex
+++ apps/heroes_server/lib/game.ex
@@ -28,103 +28,48 @@ defmodule Game do
   @moduledoc """
   Players entrypoint to the game.
   """
+  @behaviour GameBehaviour
 
-  use GenServer
-
-  alias Game.{HeroServer, HeroSupervisor}
-
-  @typedoc """
-  Unique identifier for every active player.
-
-  Base 32 hex encoded string of 26 characters.
-  """
-  @type player_id :: <<_::208>>
-
-  @typep state :: %__MODULE__.State{
-           board: module(),
-           dice: function()
-         }
-
-  defmodule State do
-    @moduledoc false
-
-    @enforce_keys [:board, :dice]
-
-    defstruct @enforce_keys
-  end
-
-  ## Client
-
-  @doc """
-  Start the server entrypoint.
-
-  Requires to be configured with
-  `:board` and `:player_spawn`.
-
-  Optionally can receive a `:name`.
-  """
-  @spec start_link(keyword()) :: GenServer.on_start()
-  def start_link(opts) do
-    {name, opts} = Keyword.pop(opts, :name, __MODULE__)
-    GenServer.start_link(__MODULE__, opts, name: name)
-  end
-
-  @doc """
-  Join a player to the game creating a new hero.
-  """
-  @spec join :: player_id
-  def join, do: GenServer.call(__MODULE__, :join)
-
-  @doc """
-  Hero process name.
-  """
-  @spec hero(player_id) :: {:via, module(), term()}
-  def hero(id), do: {:via, Registry, {Game.Registry, id}}
-
-  @doc """
-  Remove a player's hero from the game.
-  """
-  @spec remove(player_id) :: {:ok, pid()}
-  def remove(id) do
-    server = hero(id)
-    Task.start(GenServer, :stop, [server])
-  end
-
-  ## Server (callbacks)
+  alias Game.{Board, HeroServer, HeroSupervisor}
 
   @impl true
-  @spec init(keyword()) :: {:ok, state}
-  def init(opts) do
-    board = Keyword.fetch!(opts, :board)
-
-    dice =
-      case Keyword.fetch!(opts, :player_spawn) do
-        :randomized -> &Enum.random/1
-        :first_tile -> &Kernel.hd/1
-      end
-
-    {:ok, %State{board: board, dice: dice}}
-  end
-
-  @impl true
-  def handle_call(:join, _from, %State{board: board, dice: dice} = state) do
+  def join(board, dice) do
     player_id = generate_id()
-    tiles = board.tiles()
+    tile = choose_tile(board, dice)
 
     opts = [
-      name: hero(player_id),
+      name: {:via, Registry, {Game.Registry, player_id, tile}},
       board: board,
-      tile: dice.(tiles)
+      tile: tile
     ]
 
     {:ok, _pid} = DynamicSupervisor.start_child(HeroSupervisor, {HeroServer, opts})
 
-    {:reply, player_id, state}
+    player_id
   end
 
-  @spec generate_id :: player_id
+  @impl true
+  def remove(id) do
+    server = {:via, Registry, {Game.Registry, id}}
+    {:ok, _pid} = Task.start(GenServer, :stop, [server])
+    :ok
+  end
+
+  @impl true
+  def position(id) do
+    [{_pid, position}] = Registry.lookup(Game.Registry, id)
+    position
+  end
+
+  @spec generate_id :: binary()
   defp generate_id do
     random_bytes = :crypto.strong_rand_bytes(16)
     Base.hex_encode32(random_bytes, padding: false)
+  end
+
+  @spec choose_tile(module(), fun()) :: Board.tile()
+  defp choose_tile(board, dice) do
+    tiles = board.tiles()
+    dice.(tiles)
   end
 end
