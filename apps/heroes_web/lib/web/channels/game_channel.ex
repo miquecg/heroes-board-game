6ae58c09ee3288b516b722f90791c05dc6a733d0
Miguel C <miquecg@users.noreply.github.com>
2020-09-29 15:39:49 +0200

    Update game channel implementation to pass tests

--- apps/heroes_web/lib/web/channels/game_channel.ex
+++ apps/heroes_web/lib/web/channels/game_channel.ex
@@ -5,35 +5,55 @@ defmodule Web.GameChannel do
 
   use HeroesWeb, :channel
 
+  alias Game.Board
   alias Phoenix.Socket
   alias Web.Presence
 
   @impl true
-  def join("game:board", _message, %{assigns: %{player_id: id, game: game}} = socket) do
-    if authorized?(socket) do
-      {_, _} = position = game.position(id)
-      send(self(), {:after_join, position})
-      {:ok, socket}
-    else
-      {:error, %{reason: "unauthorized"}}
+  def join("game:board", _msg, %{assigns: %{game: game, player: player}} = socket) do
+    case game.position(player) do
+      {_x, _y} = position ->
+        send(self(), {:after_join, position})
+        hero = %{hero: hero_id()}
+        {:ok, hero, assign(socket, hero)}
+
+      {} ->
+        {:error, %{reason: "game over"}}
     end
   end
 
   @impl true
-  def handle_info({:after_join, {x, y}}, %{assigns: %{player_id: id}} = socket) do
+  def handle_info({:after_join, position}, socket) do
     push(socket, "presence_state", Presence.list(socket))
 
-    {:ok, _} = Presence.track(socket, id, %{x: x, y: y})
-    {:ok, _} = Presence.track(self(), "game:lobby", id, %{})
-
-    {:noreply, socket}
-  end
-
-  @spec authorized?(Socket.t()) :: boolean()
-  defp authorized?(socket) do
-    case Presence.get_by_key(socket, socket.assigns.player_id) do
-      [] -> true
-      %{metas: _} -> false
+    with {:ok, _} <- authorize(socket),
+         {:ok, _} <- track_hero(socket, position) do
+      no_reply(socket)
+    else
+      {:error, _} -> shutdown(socket)
     end
   end
+
+  @spec hero_id :: binary()
+  defp hero_id do
+    bytes = :crypto.strong_rand_bytes(8)
+    Base.encode16(bytes, case: :lower)
+  end
+
+  @spec authorize(Socket.t()) :: {:ok, ref :: binary()} | {:error, reason :: term()}
+  defp authorize(%{assigns: %{player: id}}) do
+    case Presence.get_by_key("game:lobby", id) do
+      [] -> Presence.track(self(), "game:lobby", id, %{})
+      _ -> {:error, :max_connections}
+    end
+  end
+
+  @spec track_hero(Socket.t(), Board.tile()) ::
+          {:ok, ref :: binary()} | {:error, reason :: term()}
+  defp track_hero(%{assigns: %{hero: id}} = socket, {x, y}) do
+    Presence.track(socket, id, %{x: x, y: y})
+  end
+
+  defp no_reply(socket), do: {:noreply, socket}
+  defp shutdown(socket), do: {:stop, :shutdown, socket}
 end
