783951bb08d13f0d62437907b187429b7437c72f
Miguel C <miquecg@users.noreply.github.com>
2020-12-27 13:53:21 +0100

    Upgrade wallaby to 0.28.0 version

--- apps/heroes_web/mix.exs
+++ apps/heroes_web/mix.exs
@@ -31,7 +31,7 @@ defmodule HeroesWeb.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:wallaby, "~> 0.26.0", only: :test, runtime: false},
+      {:wallaby, "~> 0.28.0", only: :test, runtime: false},
       {:hammox, "~> 0.2.5", only: :test}
     ]
   end
