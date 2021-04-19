cfe49061be0f1cbda418501837bc70411395a689
Miguel C <miquecg@users.noreply.github.com>
2021-04-19 19:11:19 +0200

    Redirect any HTTP request to HTTPS

--- config/prod.exs
+++ config/prod.exs
@@ -1,6 +1,7 @@
 import Config
 
 config :heroes_web, Web.Endpoint,
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 config :logger, level: :info
