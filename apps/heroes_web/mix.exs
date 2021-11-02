f1a10fc57acbcfe261b996b5f4bb2160338ee4ac
Miguel C <miquecg@users.noreply.github.com>
2021-11-02 13:59:35 +0100

    Update deps for phoenix 1.6 migration

--- apps/heroes_web/mix.exs
+++ apps/heroes_web/mix.exs
@@ -31,9 +31,10 @@ defmodule HeroesWeb.MixProject do
       {:hammox, "~> 0.5", only: :test},
       {:heroes_server, in_umbrella: true},
       {:jason, "~> 1.0"},
-      {:phoenix, "~> 1.5.3"},
-      {:phoenix_html, "~> 3.0"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:phoenix, "~> 1.6"},
+      {:phoenix_html, "~> 3.1"},
+      {:phoenix_live_reload, "~> 1.3", only: :dev},
+      {:phoenix_live_view, "~> 0.17.4"},
       {:plug_cowboy, "~> 2.0"},
       {:wallaby, "~> 0.29.0", only: :test, runtime: false}
     ]
