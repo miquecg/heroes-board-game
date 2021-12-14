63fbc2223ef579ab41f4e5cd065626634b6aae92
Miguel C <miquecg@users.noreply.github.com>
2021-12-14 20:14:14 +0100

    Change Erlang and Elixir version requirements
    Erlang 24.1.7
    Elixir 1.13

--- apps/heroes_web/mix.exs
+++ apps/heroes_web/mix.exs
@@ -9,7 +9,7 @@ defmodule HeroesWeb.MixProject do
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
-      elixir: "~> 1.12",
+      elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers(),
       start_permanent: Mix.env() == :prod,
